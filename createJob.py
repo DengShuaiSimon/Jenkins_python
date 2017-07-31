e284ef73d9cb1f2bcdba72c712f9def038144016
DengShuaiSimon <denfshuaishuai@icloud.com>
2017-07-31 09:44:07 +0800

    Update createJob.py

--- createJob.py
+++ createJob.py
@@ -3,11 +3,15 @@ from __future__ import print_function
 from jenkinsapi.jenkins import Jenkins
 import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
 jenkins_url = "http://localhost:8080/"
 ###jenkins_url = "http://dengshuai_super:8080@127.0.0.1:8080"
 jenkins = Jenkins('http://localhost:8080')
 #jenkins = Jenkins(jenkins_url,username="dengshuai_super",password="8080")
-job_name = 'foo_job2'
+job_name = 'test6'#foo_job1
 
 ###xml = resource_string('examples', 'addjob.xml')
 
@@ -31,17 +35,16 @@ print(my_job)
 
 # Create ListView in main view
 logger.info('Attempting to create new view')
-test_view_name = 'SimpleListView2'
+test_view_name = 'SimpleListView1'
 my_view = jenkins.views.create(test_view_name)
 my_view.add_job(job_name, my_job)
 
-assert len(my_view) == 1
+#assert len(my_view) == 1   ##len(my_view) can get the number of jobs in the view
 
-logger.info('Attempting to delete view that already exists')
-del jenkins.views[test_view_name]
+#logger.info('Attempting to delete view that already exists')
+#del jenkins.views[test_view_name]
 
-if test_view_name in jenkins.views:
-    logger.error('View was not deleted')
-else:
-    logger.info('View has been deleted')
-
+#if test_view_name in jenkins.views:
+#    logger.error('View was not deleted')
+#else:
+#    logger.info('View has been deleted')
